c0d0364fb3abf3561437d76c3851f43ec5b42567
asad9721 <mehmood9721@gmail.com>
2024-11-26 01:30:50 -0500

    Added link to render in the readme.md file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # streamlit-dashboard
 My first project
+https://streamlit-dashboard-s9ry.onrender.com
--- app.py
+++ app.py
@@ -1,5 +1,40 @@
+import pandas as pd
+import plotly.express as px
 import streamlit as st
+import scipy.stats
+import time
 
-st.header('Tossing a Coin')
+df = pd.read_csv('vehicles_us.csv')
+model_year_medians = df.groupby('model')['model_year'].median()
+df['model_year'] = df['model_year'].fillna(df['model'].map(model_year_medians))
 
-st.write('It is not a functional application yet. Under construction.')
+cylinders_medians = df.groupby('model_year')['cylinders'].median()
+df['cylinders'] = df['cylinders'].fillna(df['model_year'].map(cylinders_medians))
+
+df['odometer'] = df['odometer'].fillna(df['odometer'].median())
+
+df['paint_color'] = df['paint_color'].fillna('unknown')
+df['is_4wd'] = df['is_4wd'].fillna(0)
+
+fig = px.histogram(df, x='price', nbins=50, title='Distribution of Vehicle Prices')
+fig.update_xaxes(title='Price')
+fig.update_yaxes(title='Count')
+st.plotly_chart(fig)
+
+fig = px.scatter(df, x='odometer', y='price', color='condition', 
+   title='Price vs. Odometer (Colored by Condition)')
+fig.update_xaxes(title='Odometer (miles)')
+fig.update_yaxes(title='Price')
+st.plotly_chart(fig)
+
+avg_price_by_type = df.groupby('type')['price'].mean().reset_index()
+fig = px.bar(avg_price_by_type, x='type', y='price', title='Average Price by Vehicle Type')
+fig.update_xaxes(title='Vehicle Type')
+fig.update_yaxes(title='Average Price')
+st.plotly_chart(fig)
+
+fig = px.line(df.groupby('model_year')['price'].mean().reset_index(), 
+ x='model_year', y='price', title='Trend of Average Price by Model Year')
+fig.update_xaxes(title='Model Year')
+fig.update_yaxes(title='Average Price')
+st.plotly_chart(fig)
